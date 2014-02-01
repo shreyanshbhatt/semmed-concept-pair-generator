19493cda03a594ef0ddcba900337f945d4021c57
Alan Smith <alan@knoesis.org>
2014-01-31 20:33:44 -0500

    add config-by-xml with sample config

--- src/main/java/org/knoesis/semmed/concept/Driver.java
+++ src/main/java/org/knoesis/semmed/concept/Driver.java
@@ -1,9 +1,31 @@
 package org.knoesis.semmed.concept;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+
 public class Driver {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final String DEFAULT_DB_DRIVER = "com.db.jdbc.Driver";
+
+    private static final String KEY_DB_DRIVER = "db.driverclass";
+    private static final String KEY_DB_DATABASE = "db.database";
+    private static final String KEY_DB_USER = "db.user";
+    private static final String KEY_DB_PASSWORD = "db.password";
+
+    public static void main(String[] args) throws ConfigurationException {
+
+        if (args.length < 1) {
+            throw new ConfigurationException("Configuration XML not specified.");
+        }
+
+        Configuration config = new XMLConfiguration(args[0]);
+
+        String dbDriver = config.getString(KEY_DB_DRIVER, DEFAULT_DB_DRIVER);
+        String dbDatabase = config.getString(KEY_DB_DATABASE);
+        String dbUser = config.getString(KEY_DB_USER);
+        String dbPassword = config.getString(KEY_DB_PASSWORD);
+
     }
 
 }
