97f1e342f6eb713ef642796e0db351b89c02b9a3
mo-getter <alan@knoesis.org>
2014-02-01 19:41:47 -0500

    improve robustness:
     - accept multiple input paths
     - let hadoop calculate number of reducers if not overridden by user
     - let hadoop generate job name if not specified by user
     - take advantage of GenericOptionsParser

--- src/main/java/org/knoesis/semmed/concept/Driver.java
+++ src/main/java/org/knoesis/semmed/concept/Driver.java
@@ -5,7 +5,6 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -18,12 +17,11 @@ import org.knoesis.semmed.concept.inputformat.SemMedInputFormat;
 
 public class Driver extends Configured implements Tool {
 
-    private static final String DEFAULT_DB_DRIVER = "com.db.jdbc.Driver";
-    private static final String DEFAULT_DB_TABLENAME = "CONCEPT_AGGREGATION";
+    private static final String DEFAULT_DB_DRIVER = "com.mysql.jdbc.Driver";
     private static final String DEFAULT_DB_SCHEME = "jdbc:mysql";
     private static final String DEFAULT_DB_HOST = "localhost";
     private static final int DEFAULT_DB_PORT = 3306;
-    private static final int DEFAULT_HADOOP_NUM_REDUCERS = 4;
+    private static final int DEFAULT_NUM_REDUCERS = -1;
 
     private static final String KEY_DB_DRIVER = "db.driverclass";
     private static final String KEY_DB_SCHEME = "db.scheme";
@@ -35,17 +33,13 @@ public class Driver extends Configured implements Tool {
     private static final String KEY_DB_TABLENAME = "db.tablename";
     private static final String KEY_HADOOP_JOBNAME = "hadoop.jobname";
     private static final String KEY_HADOOP_NUM_REDUCERS = "hadoop.numreducers";
-    private static final String KEY_HADOOP_INPUT_DIR = "hadoop.inputdir";
+    private static final String KEY_HADOOP_INPUT_DIRS = "hadoop.inputdirs";
 
-    private static final String[] DB_FIELDS = {
-        "PMID",
-        "SID",
-        "s_cui",
-        "o_cui"
-    };
+    private static final int NUM_DB_FIELDS = 4;
 
     public static void main(String[] args) throws Exception {
-        ToolRunner.run(new Driver(), args);
+        int res = ToolRunner.run(new Configuration(), new Driver(), args);
+        System.exit(res);
     }
 
     public int run(String[] args) throws Exception {
@@ -53,39 +47,47 @@ public class Driver extends Configured implements Tool {
             throw new ConfigurationException("Configuration XML not specified.");
         }
 
-        XMLConfiguration userConf = new XMLConfiguration(args[0]);
-        String dbDriver = userConf.getString(KEY_DB_DRIVER, DEFAULT_DB_DRIVER);
-        String dbScheme = userConf.getString(KEY_DB_SCHEME, DEFAULT_DB_SCHEME);
-        String dbHost = userConf.getString(KEY_DB_HOST, DEFAULT_DB_HOST);
-        int dbPort = userConf.getInt(KEY_DB_PORT, DEFAULT_DB_PORT);
-        String dbDatabase = userConf.getString(KEY_DB_DATABASE);
-        String dbUser = userConf.getString(KEY_DB_USER);
-        String dbPassword = userConf.getString(KEY_DB_PASSWORD);
-        String dbTableName = userConf.getString(KEY_DB_TABLENAME, DEFAULT_DB_TABLENAME);
+        XMLConfiguration xmlConf = new XMLConfiguration(args[0]);
+        String dbDriver = xmlConf.getString(KEY_DB_DRIVER, DEFAULT_DB_DRIVER);
+        String dbScheme = xmlConf.getString(KEY_DB_SCHEME, DEFAULT_DB_SCHEME);
+        String dbHost = xmlConf.getString(KEY_DB_HOST, DEFAULT_DB_HOST);
+        int dbPort = xmlConf.getInt(KEY_DB_PORT, DEFAULT_DB_PORT);
+        String dbDatabase = xmlConf.getString(KEY_DB_DATABASE);
+        String dbUser = xmlConf.getString(KEY_DB_USER);
+        String dbPassword = xmlConf.getString(KEY_DB_PASSWORD);
+        String dbTableName = xmlConf.getString(KEY_DB_TABLENAME);
 
         Configuration conf = getConf();
         String dbUrl = new URI(String.format("%s://%s:%d/%s", dbScheme, dbHost, dbPort, dbDatabase)).toString();
         DBConfiguration.configureDB(conf, dbDriver, dbUrl, dbUser, dbPassword);
 
-        String jobName = userConf.getString(KEY_HADOOP_JOBNAME);
-        int numReducers = userConf.getInt(KEY_HADOOP_NUM_REDUCERS, DEFAULT_HADOOP_NUM_REDUCERS);
-        String inputDir = userConf.getString(KEY_HADOOP_INPUT_DIR);
-
-        Job job = Job.getInstance(conf, jobName);
-        FileInputFormat.addInputPath(job, new Path(inputDir));
-        DBOutputFormat.setOutput(job, dbTableName, DB_FIELDS);
-        job.setOutputFormatClass(DBOutputFormat.class);
+        String jobName = xmlConf.getString(KEY_HADOOP_JOBNAME, "");
+        int numReducers = xmlConf.getInt(KEY_HADOOP_NUM_REDUCERS, DEFAULT_NUM_REDUCERS);
+        String inputDirs = xmlConf.getString(KEY_HADOOP_INPUT_DIRS);
+ 
+        Job job = Job.getInstance(conf);
+        
+        FileInputFormat.setInputPaths(job, inputDirs);
+        DBOutputFormat.setOutput(job, dbTableName, NUM_DB_FIELDS);
+        
+        if (!jobName.isEmpty()) {
+            job.setJobName(jobName);
+        }
+        job.setJarByClass(Driver.class);
         job.setInputFormatClass(SemMedInputFormat.class);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(ConceptCoocurrence.class);
+        job.setOutputFormatClass(DBOutputFormat.class);
         job.setOutputKeyClass(ConceptCoocurrence.class);
         job.setOutputValueClass(NullWritable.class);
-        job.setJarByClass(Driver.class);
         job.setMapperClass(ConceptMapper.class);
         job.setReducerClass(ConceptReducer.class);
-        job.setNumReduceTasks(numReducers);
+        
+        if (numReducers > 0) {
+            job.setNumReduceTasks(numReducers);
+        }
+        
         return job.waitForCompletion(true) ? 0 : 1;
-
     }
 
 }
