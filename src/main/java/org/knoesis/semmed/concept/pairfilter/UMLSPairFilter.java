4d3d0fd24b4b3cf5a09440dfba2fb99037d3ed39
Alan Smith <alan@knoesis.org>
2014-02-25 16:52:33 -0500

    load entries from SetFile into a HashSet.
    lesson learned!

--- src/main/java/org/knoesis/semmed/concept/pairfilter/UMLSPairFilter.java
+++ src/main/java/org/knoesis/semmed/concept/pairfilter/UMLSPairFilter.java
@@ -1,6 +1,8 @@
 package org.knoesis.semmed.concept.pairfilter;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.hadoop.conf.Configuration;
@@ -15,29 +17,30 @@ public class UMLSPairFilter implements PairFilter {
 
     private final SetFile.Reader set;
     private final Text value = new Text();
+    private final Set<String> validPairs = new HashSet<String>();
 
     protected UMLSPairFilter(Configuration conf) throws IOException {
         set = new SetFile.Reader(FileSystem.get(conf), conf.get(FILTER_DIR), conf);
+        Text key = new Text();
+        while (set.next(key)) {
+            validPairs.add(key.toString());
+        }
+        IOUtils.closeStream(set);
     }
 
     public boolean accept(String semType1, String semType2) {
         semType1 = semType1.toLowerCase();
         semType2 = semType2.toLowerCase();
+        StringBuilder pairBuilder;
         if (semType1.compareTo(semType2) < 0) {
-            value.set(semType1 + "|" + semType2);
+            pairBuilder = new StringBuilder(semType1).append("|").append(semType2);
         } else {
-            value.set(semType2 + "|" + semType1);
+            pairBuilder = new StringBuilder(semType2).append("|").append(semType1);
         }
-        try {
-            return set.get(value) != null;
-        } catch (IOException ex) {
-            throw new RuntimeException("Exception reading from pair filter", ex);
-        }
+        return validPairs.contains(pairBuilder.toString());
     }
 
-    public void close() {
-        IOUtils.closeStream(set);
-    }
+    public void close() {}
 
     public static PairFilter get(Configuration conf) {
         String setFilePath = conf.get(FILTER_DIR);
